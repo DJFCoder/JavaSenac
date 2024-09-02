3808704e3112410054802274349223af99301321
DJFCoder <147553362+DJFCoder@users.noreply.github.com>
2024-09-02 19:34:51 -0300

    Update Atv1_1.java

--- atv1_hotel/src/atv1_hotel/Atv1_1.java
+++ atv1_hotel/src/atv1_hotel/Atv1_1.java
@@ -2,14 +2,14 @@
 
 ENUNCIADO:
     Indique qual de seus dois auditórios é o mais adequado para um evento. O
-auditório Alfa conta com 150 lugares e espaço para até 70 cadeiras adicionais. O
-auditório Beta conta com 350 lugares, sem espaço para mais cadeiras. Desenvolva
-um programa Java que receba o número de convidados do evento e faça uma
-verificação sobre esse número: se for maior que 350 ou menor que zero, deverá ser
-mostrada a mensagem “Número de convidados inválido”. Se o valor informado é
-válido, deverá ser mostrado na tela qual dos auditórios é o mais adequado. No caso
-do auditório Alfa, será preciso calcular ainda quantas cadeiras adicionais serão
-necessárias, observando o limite citado anteriormente.
+    auditório Alfa conta com 150 lugares e espaço para até 70 cadeiras adicionais. O
+    auditório Beta conta com 350 lugares, sem espaço para mais cadeiras. Desenvolva
+    um programa Java que receba o número de convidados do evento e faça uma
+    verificação sobre esse número: se for maior que 350 ou menor que zero, deverá ser
+    mostrada a mensagem “Número de convidados inválido”. Se o valor informado é
+    válido, deverá ser mostrado na tela qual dos auditórios é o mais adequado. No caso
+    do auditório Alfa, será preciso calcular ainda quantas cadeiras adicionais serão
+    necessárias, observando o limite citado anteriormente.
 
 */
 

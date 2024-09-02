ea516131a705849ba2b44fd39eebe00896008b47
DJFCoder <147553362+DJFCoder@users.noreply.github.com>
2024-09-02 19:34:36 -0300

    Update Atv1_2.java

--- atv1_hotel/src/atv1_hotel/Atv1_2.java
+++ atv1_hotel/src/atv1_hotel/Atv1_2.java
@@ -2,14 +2,14 @@
 
 ENUNCIADO:
     Escreva um programa Java que faça a troca de quartos entre dois hospedes. É
-preciso que ele receba o nome de um cliente e sua idade; o cliente a princípio será
-hospedado no quarto A. Depois, o programa deve receber o nome de outro cliente
-e sua idade; se o cliente for mais jovem, ficará no quarto B, mas, se a idade deste
-for maior que a do primeiro cliente, ele ficará no quarto A e o primeiro cliente ficará
-no quarto B. Além disso, se a pessoa mais velha for idosa (com 60 anos ou mais),
-terá desconto de 40%. O programa deve mostrar ao final o nome do cliente que
-ficou no quarto A e o desconto, se houver, e o nome do cliente que ficou no quarto
-B.
+    preciso que ele receba o nome de um cliente e sua idade; o cliente a princípio será
+    hospedado no quarto A. Depois, o programa deve receber o nome de outro cliente
+    e sua idade; se o cliente for mais jovem, ficará no quarto B, mas, se a idade deste
+    for maior que a do primeiro cliente, ele ficará no quarto A e o primeiro cliente ficará
+    no quarto B. Além disso, se a pessoa mais velha for idosa (com 60 anos ou mais),
+    terá desconto de 40%. O programa deve mostrar ao final o nome do cliente que
+    ficou no quarto A e o desconto, se houver, e o nome do cliente que ficou no quarto
+    B.
 
 */
 

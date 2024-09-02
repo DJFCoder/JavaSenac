32700851dbe0b9b164b49c03b57b3ccdd308c0e5
DJFCoder <147553362+DJFCoder@users.noreply.github.com>
2024-09-02 19:34:21 -0300

    Update Atv1_3.java

--- atv1_hotel/src/atv1_hotel/Atv1_3.java
+++ atv1_hotel/src/atv1_hotel/Atv1_3.java
@@ -2,14 +2,14 @@
 
 ENUNCIADO:
     Escreva um programa Java que receba o valor padrão de uma diária e em
-seguida receba vários nomes de hóspedes e suas idades. Caso a idade do
-hóspede seja menor de 4 anos, ele não paga hospedagem – nesses casos, é
-preciso que seja mostrada na tela a mensagem “(Nome do hóspede) possui
-gratuidade”. Hóspedes com mais de 80 anos pagam metade – então dever ser
-mostrado na tela “(Nome do hóspede) paga meia”. O usuário informará hóspedes
-até digitar a palavra “PARE”, que interrompe a entrada de dados. Ao fim, o
-programa deve mostrar a quantidade de gratuidades, a quantidade de meias
-hospedagens e o valor total, considerando todos os hóspedes informados.
+    seguida receba vários nomes de hóspedes e suas idades. Caso a idade do
+    hóspede seja menor de 4 anos, ele não paga hospedagem – nesses casos, é
+    preciso que seja mostrada na tela a mensagem “(Nome do hóspede) possui
+    gratuidade”. Hóspedes com mais de 80 anos pagam metade – então dever ser
+    mostrado na tela “(Nome do hóspede) paga meia”. O usuário informará hóspedes
+    até digitar a palavra “PARE”, que interrompe a entrada de dados. Ao fim, o
+    programa deve mostrar a quantidade de gratuidades, a quantidade de meias
+    hospedagens e o valor total, considerando todos os hóspedes informados.
 
 */
 
